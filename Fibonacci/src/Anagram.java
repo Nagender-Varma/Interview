bd5b692fca2bb2b39c0ca94a9ec90de5157ea74d
nagender varma <nagendervarma5@gmail.com>
2021-06-29 14:23:34 +0530

    Anagram code added

---
+++ Fibonacci/src/Anagram.java
@@ -0,0 +1,31 @@
+package Fibonacci.src;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Anagram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 1st word :");
+        String s1= sc.nextLine();
+        System.out.println("Enter 2nd word :");
+        String s2= sc.nextLine();
+        char ch1[] = s1.toCharArray();
+        char ch2[] = s2.toCharArray();
+        sc.close();
+        System.out.println(Anagram.solve(ch1, ch2));
+
+       
+    }
+
+    private static boolean solve(char[] ch1, char[] ch2) {
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+        if (Arrays.equals(ch1, ch2)) {
+            return true;
+        }
+
+        return false;
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Patterns
-Fibonacci Pattern in right angle triangle.
+Fibonacci Pattern in right angle triangle. June 18th 2021
+Anagram June 28th 2021
