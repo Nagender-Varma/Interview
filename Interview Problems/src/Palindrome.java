20b538ef7d3fb3ee5ac68977935d8c736eba71fa
Nagender-Varma <55315282+Nagender-Varma@users.noreply.github.com>
2022-10-28 22:15:05 +0530

    Palindrome code added (#5)

---
+++ Interview Problems/src/Palindrome.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Palindrome {
+    public static void main(String[] args) throws Exception {
+      int remainder, sum =0, temp;
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter a number to check if it is palindrome or not : ");
+      int n = sc.nextInt();
+      temp = n;
+      while (n>0){
+        remainder = n%10;
+        sum = (sum*10)+remainder;
+        n = n/10;
+      }
+      if(temp == sum){
+        System.out.println("Given number is a Palindrome");
+        
+      }else {
+        System.out.println("Given number is not a palindrome");
+      }
+    }
+}
+
