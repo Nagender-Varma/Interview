b2d82ec1c983a8f092630cf8fce0a71d5cdd46b0
nagender varma <nagendervarma5@gmail.com>
2022-10-28 22:26:00 +0530

    scanner close

--- Interview Problems/src/Palindrome.java
+++ Interview Problems/src/Palindrome.java
@@ -3,14 +3,16 @@ public class Palindrome {
     public static void main(String[] args) throws Exception {
       int remainder, sum =0, temp;
       Scanner sc = new Scanner(System.in);
-      System.out.println("Enter a number to check if it is palindrome or not : ");
-      int n = sc.nextInt();
-      temp = n;
-      while (n>0){
-        remainder = n%10;
-        sum = (sum*10)+remainder;
-        n = n/10;
-      }
+        System.out.println("Enter a number to check if it is palindrome or not : ");
+          int n = sc.nextInt();
+          sc.close();
+          temp = n;
+          while (n>0){
+            remainder = n%10;
+            sum = (sum*10)+remainder;
+            n = n/10;
+          }
+    
       if(temp == sum){
         System.out.println("Given number is a Palindrome");
         
