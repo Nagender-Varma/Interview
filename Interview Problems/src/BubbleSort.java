d6d08c4ca8eb523c4186656ee6626d612610382c
nagender varma <nagendervarma5@gmail.com>
2025-07-27 16:53:39 +0530

    Bubble Sort algorithm

---
+++ Interview Problems/src/BubbleSort.java
@@ -0,0 +1,46 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+
+        int[] arr = {5, 3, 4, 1, 2};
+        System.out.println("Original Array:");
+        printArray(arr);
+        System.out.println("Bubble Sort Execution Steps");
+        bubbleSort(arr);
+        System.out.println("Sorted Array");
+        printArray(arr);
+
+    }
+
+    private static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + "");
+        }
+        System.out.println();
+    }
+
+    private static void bubbleSort(int[] arr) {
+        int pass = 1;
+        int lengthOfArray = arr.length;
+        for (int i = 0; i < lengthOfArray - 1; i++) {
+            System.out.println("Outer Loop Pass  " + pass);
+            pass++;
+
+            for (int j = 0; j < lengthOfArray - i - 1; j++) {
+                System.out.println("Inner Loop Iteration (i = " +
+                        i + ", j =" + j + ")");
+                if(arr[j]>arr[j+1]){
+                    System.out.println("Swapping " + arr[j] + " and " + arr[j+1]);
+                    int temp = arr[j];
+                    arr[j]= arr[j+1];
+                    arr[j+1] = temp;
+                }
+                printArray(arr);
+            }
+            System.out.println();
+
+        }
+
+
+    }
+}
+
